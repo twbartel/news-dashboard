129b8b8288c010c715db42022a822bc930bc26b6
Thomas Bartel <tom@tombartel.de>
2019-05-16 06:52:16 +0200

    Add Postgres configuration

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,20 @@
-require('dotenv').config();
-
-module.exports = {
-    client: 'mysql',
-    connection: {
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
-    },
-    pool: { min: 1, max: 100 }
-};
+if (process.env.DATABASE_URL) {
+    // We are running on Heroku
+    module.exports = {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: { min: 1, max: 100 }
+    };
+} else {
+    // Local MySQL
+    require('dotenv').config();
+    module.exports = {
+        client: 'mysql',
+        connection: {
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE
+        },
+        pool: { min: 1, max: 100 }
+    };
+}
